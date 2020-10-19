fb474038a6a4e360c7aadd1d7b644dad031c4a8b
Simon Bk <bk.simon50@gmail.com>
2020-10-20 00:05:58 +0300

    Add graphql basic functionality for presenting
    Add model
    Add repo
    Add service
    Add graphql schema

---
+++ meetup-graphql-impl/src/main/java/org/example/graphql/query/VehicleQuery.java
@@ -0,0 +1,25 @@
+package org.example.graphql.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.example.graphql.model.Vehicle;
+import org.example.graphql.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class VehicleQuery implements GraphQLQueryResolver {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    public List<Vehicle> getVehicles(final int count) {
+        return this.vehicleService.getAllVehicles(count);
+    }
+
+    public Optional<Vehicle> getVehicle(final int id) {
+        return this.vehicleService.getVehicle(id);
+    }
+}
---
+++ meetup-graphql-impl/src/main/java/org/example/graphql/repository/VehicleRepository.java
@@ -0,0 +1,9 @@
+package org.example.graphql.repository;
+
+import org.example.graphql.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+}
